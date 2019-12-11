bd75b520621d5aae5cdbb06f1f2429e72e97fa62
Christian Jimenez <christianjimenez0123@gmail.com>
2019-12-11 20:45:54 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears VALUES (1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0);
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 20, "M", "dark brown","calm", 0);
 INSERT INTO bears VALUES (2,"Rowdy", 10, "M", "black", "intense", 1);
 INSERT INTO bears VALUES (3,"Tabitha", 6, "F", "dark brown", "Nice", 1);
 INSERT INTO bears VALUES (4,"Sargeant Brown", 19, "M", "Green", "Slimy", 0);
 INSERT INTO bears VALUES (5,"Melissa", 13, "F", "Light brown", "goofy", 1);
-INSERT INTO bears VALUES (6,"Grinch", 2, "M", "Brown", "Grinchy", 1);
+INSERT INTO bears VALUES (6,"Grinch", 2, "M", "Brown", "vile", 1);
 INSERT INTO bears VALUES (7,"Wendy", 6, "F", "Black", "naive", 1);
 INSERT INTO bears VALUES (8,null, 20, "M", "Black", "aggressive", 0);
